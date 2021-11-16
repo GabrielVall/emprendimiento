43d43126a10d8a1817b3903200419550e5e07a29
GabrielV <gabrielvallejo2000@gmail.com>
2021-11-15 21:01:31 -0600

    Corrección de extensión pdf

--- php/v/2/proyectos.php
+++ php/v/2/proyectos.php
@@ -32,7 +32,7 @@ $total_proyecto = count($row_proyecto);
     if($total_proyecto > 0){
         for ($i=0; $i < $total_proyecto; $i++) { ?>
         
-        <a class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800" href="generar_pdf?id=<?php echo $row_proyecto[$i]['id_proyecto'] ?>" target="_blank">
+        <a class="flex items-center p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800" href="generar_pdf.php?id=<?php echo $row_proyecto[$i]['id_proyecto'] ?>" target="_blank">
                 <div class="p-3 mr-4 text-orange-500 bg-orange-100 rounded-full dark:text-orange-100 dark:bg-orange-500">
                     <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
                     <path d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01"></path>
