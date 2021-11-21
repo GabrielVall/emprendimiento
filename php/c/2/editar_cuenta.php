174a9def6d935d506fe090138b0b83a50ef82905
GabrielV <gabrielvallejo2000@gmail.com>
2021-11-21 13:19:32 -0600

    Seguirdad de certificados

--- php/c/2/editar_cuenta.php
+++ php/c/2/editar_cuenta.php
@@ -2,17 +2,18 @@
 session_start();
 include_once("../../m/SQLConexion.php");
 $sql = new SQLConexion();
-$rpta = $sql->actualizarDatos("CALL sp_update_cuenta('".$_SESSION['id_usuario_curso']."','".$_POST['nombre']."')");
+// $rpta = $sql->actualizarDatos("CALL sp_update_cuenta('".$_SESSION['id_usuario_curso']."','".$_POST['nombre']."')");
 
-if($rpta){
-    $response_array['status'] = 'success';
-    $response_array['header'] = 'Cambios registrados';
-    $response_array['msg'] = 'Se ha cambiado la información de la cuenta';
-}else{
+// if($rpta){
+    // $response_array['status'] = 'success';
+    // $response_array['header'] = 'Cambios registrados';
+    // $response_array['msg'] = 'Se ha cambiado la información de la cuenta';
+// }else{
     $response_array['status'] = 'error';
-    $response_array['header'] = 'Error de conexión';
-    $response_array['msg'] = 'Ha ocurrido un error, intenta más tarde...';
-}
+    // $response_array['header'] = 'Error de conexión';
+    $response_array['header'] = 'Esta opción fue desactivada, comuinicate con el soporte para realizar cambios.';
+    // $response_array['msg'] = 'Ha ocurrido un error, intenta más tarde...';
+// }
 
 header('Content-type: application/json');
 echo json_encode($response_array);
